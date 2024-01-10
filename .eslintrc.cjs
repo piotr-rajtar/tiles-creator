3e3c196cb243a07ca7a1322467e811c7c59bccc6
Piotr Rajtar <piotrek.rajtar@gmail.com>
2024-01-10 12:58:38 +0100

    app init

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto',
+        singleQuote: true,
+        semi: true,
+        tabWidth: 2,
+        printWidth: 100,
+      },
+    ],
+  },
+};
---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Tile Creator</h1>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" module="style"></style>
